71cc0b271ae1176428bbc42332cc8145c005939f
Aman Agarwal <aman.agarwal97@gmail.com>
2016-12-11 08:42:12 +0000

    Added Comments

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+    // configuration
+    require("../includes/config.php"); 
+    
+    //Select all stocks from portfolios
+    $rows=CS50::query("SELECT * FROM portfolios WHERE user_id=?",$_SESSION["id"]);
+    $positions=[];
+    
+    //create a array that contains details along with current stock prices
+    foreach ($rows as $row)
+    {
+        $stock=lookup($row["symbol"]);
+        if($stock!==false)
+        {
+            $positions[]=[ "name" => $stock["name"],
+                        "price" => $stock["price"],
+                        "shares" => $row["share"],
+                        "symbol" => $row["symbol"]
+                        ];
+            
+        }
+        
+        
+    }
+    
+    //To determine the cash the user has
+    $balance=CS50::query("SELECT cash FROM users WHERE id=?",$_SESSION["id"]);
+    render("portfolio.php", ["positions" => $positions , "title" => "Portfolios","balance" => $balance[0]["cash"]]);
+    
+
+?>
