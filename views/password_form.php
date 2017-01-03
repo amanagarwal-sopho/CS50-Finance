604f7c020bac8703e1d5da3eca675996b26e6dbf
Aman Agarwal <aman.agarwal97@gmail.com>
2017-01-03 16:02:43 +0000

    added another field

--- views/password_form.php
+++ views/password_form.php
@@ -6,6 +6,9 @@
         <div class="form-group">
             <input autocomplete="off" autofocus class="form-control" name="newp" placeholder="New Password" type="password"/>
         </div>
+        div class="form-group">
+            <input autocomplete="off" autofocus class="form-control" name="confirmp" placeholder="Confirm Password" type="password"/>
+        </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-cog"></span>
